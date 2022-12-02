8920ddd57d5a87069f1a30c68d15dba14d22e042
Jonas Batista <jonas.nb4@gmail.com>
2022-12-02 15:23:23 -0300

    add name

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import Card from "./components/Card";
 
 const App = () => {
   return (
-    <div className="h-screen">
+    <div className="h-full mt-16">
       <Card />
+      <h1 className="text-center pt-10 text-emerald-400">
+        Create with 💙 by Jonas Batista
+      </h1>
     </div>
   );
 };
